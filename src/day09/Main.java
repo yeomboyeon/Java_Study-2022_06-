9229be83d55eed7a7788ed6f6b24ddb69b1cab6e
yeomboyeon <yeomboyeon1221@gmail.com>
2022-07-11 18:36:40 +0900

    메서드 오버라이딩과 접근지정자

--- src/day09/Main.java
+++ src/day09/Main.java
@@ -1,43 +1,32 @@
 package day09;
 
-/*메서드 오버라이딩 vs. 메서드 오버로딩*/
+/*메서드 오버라이딩과 접근지정자*/
 
 class A {
-	void print1() {
-		System.out.println("A 클래스 print1");
-	}
-	void print2() {
-		System.out.println("A 클래스 print2");
-	}
-}
-class B extends A {
-	@Override
-	void print1() {
-		System.out.println("B 클래스 print1");
-	}
-	void print2(int a) {
-		System.out.println("B 클래스 print2");
-	}
-}
-public class Main {
-	public static void main(String[] args) {		
-		//#1. A 타입 선언 / A 생성자 사용
-		A aa = new A();
-		aa.print1(); //A 클래스 print1
-		aa.print2(); //A 클래스 print2
-		System.out.println();
-		
-		//#2. B 타입 선언 / B 생성자 사용
-		B bb = new B();
-		bb.print1(); // B 클래스 print1
-		bb.print2(); // A 클래스 print2
-		bb.print2(3);// B 클래스 print2
-		System.out.println();
-		
-		//#3. A 타입 선언 / B 생성자 사용
-		A ab = new B();
-		ab.print1(); //B 클래스 print1
-		ab.print2(); //A 클래스 print2
+	protected void abc() {
 	}
 }
 
+class B1 extends A {
+	public void abc() {
+	}
+}
+
+class B2 extends A {
+	protected void abc() {
+	}
+}
+
+class B3 extends A {
+	// void abc() {} //default 접근지정자 (좁아져서 불가능)
+}
+
+class B4 extends A {
+	// private void abc() {} //private 접근지정자 (좁아져서 불가능)
+}
+
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
