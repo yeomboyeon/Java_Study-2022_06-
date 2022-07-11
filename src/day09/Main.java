7696fe884b5153f34e86080977ec677fe3b3859b
yeomboyeon <yeomboyeon1221@gmail.com>
2022-07-11 18:35:00 +0900

    메소드오버로딩과 메소드오버라이딩 비교

--- src/day09/Main.java
+++ src/day09/Main.java
@@ -1,58 +1,43 @@
 package day09;
 
-class Animal {
-	void cry() {}
-}
-class Bird extends Animal {
-	@Override
-	void cry() {
-		System.out.println("짹짹");
-	}
-}
-class Cat extends Animal {
-	@Override
-	void cry() {
-		System.out.println("야옹");
-	}
-}
-class Dog extends Animal {
-	@Override
-	void cry() {
-		System.out.println("멍멍");
-	}
-}
+/*메서드 오버라이딩 vs. 메서드 오버로딩*/
 
+class A {
+	void print1() {
+		System.out.println("A 클래스 print1");
+	}
+	void print2() {
+		System.out.println("A 클래스 print2");
+	}
+}
+class B extends A {
+	@Override
+	void print1() {
+		System.out.println("B 클래스 print1");
+	}
+	void print2(int a) {
+		System.out.println("B 클래스 print2");
+	}
+}
 public class Main {
 	public static void main(String[] args) {		
-		//#1. 각각의 타입으로 선언 + 각각의 타입으로 생성
-		Animal aa = new Animal();
-		Bird bb = new Bird();
-		Cat cc = new Cat();
-		Dog dd = new Dog();		
-		aa.cry();
-		bb.cry();
-		cc.cry();
-		dd.cry();
+		//#1. A 타입 선언 / A 생성자 사용
+		A aa = new A();
+		aa.print1(); //A 클래스 print1
+		aa.print2(); //A 클래스 print2
 		System.out.println();
 		
-		//#2. Animal 타입으로 선언 + 자식클래스 타입으로 생성
-		Animal ab = new Bird();
-		Animal ac = new Cat();
-		Animal ad = new Dog();		
-		ab.cry();
-		ac.cry();
-		ad.cry();		
+		//#2. B 타입 선언 / B 생성자 사용
+		B bb = new B();
+		bb.print1(); // B 클래스 print1
+		bb.print2(); // A 클래스 print2
+		bb.print2(3);// B 클래스 print2
 		System.out.println();
 		
-		//#3. 배열로 관리
-		Animal[] animals = {ab, ac, ad};
-		for(Animal animal : animals) {
-			animal.cry();
-		}
+		//#3. A 타입 선언 / B 생성자 사용
+		A ab = new B();
+		ab.print1(); //B 클래스 print1
+		ab.print2(); //A 클래스 print2
 	}
 }
-
-
-
-
 
